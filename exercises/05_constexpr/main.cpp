7b979012f45166c297b09e8d7ef38edb83efbcd5
A0614WEN <15595733+a0614wen@user.noreply.gitee.com>
2025-07-30 19:19:52 +0800

    cxx:05(change fibonacci algorithm)

--- exercises/05_constexpr/main.cpp
+++ exercises/05_constexpr/main.cpp
@@ -1,14 +1,19 @@
 #include "../exercise.h"
 
 constexpr unsigned long long fibonacci(int i) {
-    switch (i) {
-        case 0:
-            return 0;
-        case 1:
-            return 1;
-        default:
-            return fibonacci(i - 1) + fibonacci(i - 2);
+    if (i == 0) return 0;
+    if (i == 1) return 1;
+    
+    unsigned long long a = 0; // fib(0)
+    unsigned long long b = 1; // fib(1)
+    unsigned long long result = 0;
+    
+    for (int j = 2; j <= i; ++j) {
+        result = a + b; // fib(j) = fib(j-2) + fib(j-1)
+        a = b;          // 更新 fib(j-2) 为下一轮的 fib(j-1)
+        b = result;     // 更新 fib(j-1) 为下一轮的 fib(j)
     }
+    return result;
 }
 
 int main(int argc, char **argv) {
